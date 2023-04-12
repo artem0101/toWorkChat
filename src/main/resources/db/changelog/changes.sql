cee29c11b4d8de68a369247a3208a05de1be3ca3
AMKoryakin <koryakin1993@inbox.ru>
2023-04-12 17:58:11 +0300

    2. добавление данных

--- src/main/resources/db/changelog/changes.sql
+++ src/main/resources/db/changelog/changes.sql
@@ -1,5 +1,3 @@
-CREATE SCHEMA IF NOT EXISTS zoo;
-
 DROP TABLE IF EXISTS zoo.animal_food;
 DROP TABLE IF EXISTS zoo.animal;
 DROP TABLE IF EXISTS zoo.product;
@@ -8,6 +6,7 @@ DROP SEQUENCE IF EXISTS zoo.animal_food_seq;
 DROP SEQUENCE IF EXISTS zoo.animal_seq;
 DROP SEQUENCE IF EXISTS zoo.product_seq;
 
+CREATE SCHEMA IF NOT EXISTS zoo;
 
 CREATE TABLE zoo.product (
     id           		BIGINT PRIMARY KEY,
@@ -51,4 +50,39 @@ CREATE SEQUENCE zoo.animal_food_seq
 START 1
 INCREMENT 1
 OWNED BY zoo.animal_food.id;
+
+INSERT INTO zoo.product (id,name,amount_of_products,unit_type,"type") VALUES
+	 (1,'Морковь',44,'QUANTITY','VEGETABLE'),
+	 (2,'Мясо',144,'KILOGRAM','MEAT'),
+	 (3,'Капуста',32,'QUANTITY','VEGETABLE'),
+	 (4,'Банан',23,'QUANTITY','FRUIT'),
+	 (5,'Яблоко',0,'QUANTITY','FRUIT'),
+	 (6,'Зерно',11,'KILOGRAM','CEREALS'),
+	 (7,'Вода',11,'LITER','WATER');
+
+INSERT INTO zoo.animal (id,name,"type",is_predator) VALUES
+	 (1,'Обезьяна','MAMMAL',false),
+	 (2,'Заяц','MAMMAL',false),
+	 (3,'Орёл','BIRD',true),
+	 (4,'Тигр','MAMMAL',true),
+	 (5,'Лошадь','MAMMAL',false);
+
+INSERT INTO zoo.animal_food (id,animal_id,product_id,composition_rate) VALUES
+	 (1,1,7,3),
+	 (2,1,4,5),
+	 (3,1,5,4),
+	 (4,2,7,1),
+	 (5,2,1,3),
+	 (6,2,3,2),
+	 (7,3,7,1),
+	 (8,3,2,3),
+	 (9,3,6,1),
+	 (10,3,5,2),
+	 (11,4,7,10),
+	 (12,4,7,7),
+	 (13,5,7,11),
+	 (14,5,6,7),
+	 (15,5,5,6),
+	 (16,5,1,5);
+
 
